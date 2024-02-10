802d672404640ad9235e26c6d6f27f17cd1813f4
Incredible <Jackson19970306+1@hotmail.com>
2024-02-10 00:41:27 -0800

    df

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,12 +12,12 @@ const Home: NextPage = () => {
         <div className='inline w-full min-w-[600px] '>
           {
             // console.log(item)
-            Drive2.map((item: file) => {
+            Drive2.map((item: file, index:Number) => {
               if (item.type == "text") {
                 // console.log(item)
                 return (
                   <div className='inline-block text-center w-[90px] h-[90px] m-[10px] '>
-                    <img  src="./icon-file.png" className='w-[100%]' />
+                    <img key ={`${item.name}+${index}+${item.type}`} src="./icon-file.png" className='w-[100%]' />
                     <p className='text-sm'>{item.name}</p>
                   </div>
                 )
@@ -26,7 +26,7 @@ const Home: NextPage = () => {
                 // console.log(item)
                 return (
                   <div className='text-center inline-block w-[90px] m-[10px]  h-[90px]'>
-                    <img  src="./icon-movie.png" className='w-[100%]' />
+                    <img  key = {`${item.name}+${index}+${item.type}`} src="./icon-movie.png" className='w-[100%]' />
                     <p className='text-sm'>{item.name}</p>
                   </div>
                 )
@@ -35,7 +35,7 @@ const Home: NextPage = () => {
                 // console.log(item)
                 return (
                   <div className='text-center w-[90px] inline-block m-[10px]  h-[90px]'>
-                    <img src="./icon-json.png" className='w-[100%]' />
+                    <img  key = {`${item.name}+${index}+${item.type}`} src="./icon-json.png" className='w-[100%]' />
                     <p className='text-sm'>{item.name}</p>
                   </div>
                 )
@@ -44,7 +44,7 @@ const Home: NextPage = () => {
                 // console.log(item)
                 return (
                   <div className='text-center w-[90px] m-[10px] inline-block h-[90px]'>
-                    <img src="./icon-folder.png" className='w-[100%]' />
+                    <img  key = {`${item.name}+${index}+${item.type}`} src="./icon-folder.png" className='w-[100%]' />
                     <p className='text-sm'>{item.name}</p>
                   </div>
                 )
@@ -53,12 +53,12 @@ const Home: NextPage = () => {
           }
         </div>
         <div className='fixed bottom-0 w-full h-[30px] flex items-center pl-[20px] border-t border-solid border-1 border-gray-500'>
-          <img   src="./icon-folder.png" className='w-[15px] mr-[10px]' />
+          <img   key = {`${item.name}+${index}+${item.type}`} src="./icon-folder.png" className='w-[15px] mr-[10px]' />
           <p className='text-sm text-[gray]'>{`E : >`}</p>
         </div>
       </div>
       <div className={`min-h-[90vh] ${preview ? "w-[25%]": "hidden"}`}>
-        <img src='/preview_img.png' className='w-full p-5 h-[35%]'/>
+        <img  key = {`${item.name}+${index}+${item.type}`} src='/preview_img.png' className='w-full p-5 h-[35%]'/>
         <div className='preview_content rounded-xl bg-[#3f4563] h-[65%] flex items-center justify-center'>
           <p>Nothing selected</p>
         </div>
